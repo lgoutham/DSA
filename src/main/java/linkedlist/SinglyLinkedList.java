f0bc42a2b36e42fd162fea97c21a3c09d7407e07
Goutham <lgouthamreddy9@gmail.com>
2022-09-07 17:57:39 +0530

    SinglyLinkedList-Data structure insertInSortedList, contains, containsLoop, findStartingNodeInLoop, removeLoop, reverse, getMiddleNode, getNthNodeFromEnd, removeDuplicates, length, merge, sum

--- src/main/java/linkedlist/SinglyLinkedList.java
+++ src/main/java/linkedlist/SinglyLinkedList.java
@@ -48,6 +48,24 @@ public class SinglyLinkedList {
         tail.next = null;
     }
 
+    public void insertInSortedList(int value) {
+        Node newNode = new Node(value);
+        if (isEmpty()) head = newNode;
+        Node current = head;
+        Node temp = null;
+        while (current.data < newNode.data) {
+            temp = current;
+            current = current.next;
+        }
+        if (temp == null) {
+            head = newNode;
+            head.next = newNode;
+        } else {
+            temp.next = newNode;
+        }
+        newNode.next = current;
+    }
+
     public void delete(int value) {
         if (isEmpty()) throw new NoSuchElementException();
         if (head == tail && head.data == value) {
@@ -83,7 +101,172 @@ public class SinglyLinkedList {
         return head == null;
     }
 
-    public void print() {
+    public boolean contains(int value) {
+        Node current = head;
+        while (current != null) {
+            if (current.data == value) return true;
+            current = current.next;
+        }
+        return false;
+    }
+
+    public boolean containsLoop() {
+        if (isEmpty()) return false;
+        Node slowPtr = head, fastPtr = head;
+        while (fastPtr != null && fastPtr.next != null) {
+            fastPtr = fastPtr.next.next;
+            slowPtr = slowPtr.next;
+            if (slowPtr == fastPtr) return true;
+        }
+        return false;
+    }
+
+    public Node findStartingNodeInLoop() {
+        if (isEmpty()) return null;
+        Node slowPtr = head, fastPtr = head;
+        while (fastPtr != null && fastPtr.next != null) {
+            fastPtr = fastPtr.next.next;
+            slowPtr = slowPtr.next;
+            if (slowPtr == fastPtr) return getStartingNode(slowPtr);
+        }
+        return null;
+    }
+
+    private Node getStartingNode(Node slowPtr) {
+        Node current = head;
+        while (slowPtr != current) {
+            current = current.next;
+            slowPtr = slowPtr.next;
+        }
+        return current;
+    }
+
+    public void removeLoop() {
+        if (isEmpty()) return;
+        Node slowPtr = head, fastPtr = head;
+        while (fastPtr != null && fastPtr.next != null) {
+            fastPtr = fastPtr.next.next;
+            slowPtr = slowPtr.next;
+            if (slowPtr == fastPtr) {
+                removeLoop(slowPtr);
+                return;
+            }
+        }
+    }
+
+    private void removeLoop(Node slowPtr) {
+        Node current = head;
+        while (slowPtr.next != current.next) {
+            slowPtr = slowPtr.next;
+            current = current.next;
+        }
+        slowPtr.next = null;
+    }
+
+    public Node reverse() {
+        if (isEmpty()) return null;
+        Node current = head;
+        Node previous = null, next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        return previous;
+    }
+
+    public Node getMiddleNode() {
+        if (isEmpty()) return null;
+        Node slowPtr = head, fastPtr = head;
+        while (fastPtr != null && fastPtr.next != null) {
+            slowPtr = slowPtr.next;
+            fastPtr = fastPtr.next.next;
+        }
+        return slowPtr;
+    }
+
+    public Node getNthNodeFromEnd(int n) {
+        if (isEmpty()) return null;
+        if (n < 0) throw new IllegalArgumentException("Invalid value:n=" + n);
+        Node refPtr = head, mainPtr = head;
+        int count = 0;
+        while (count < n) {
+            if (refPtr == null) throw new IllegalArgumentException(n + " is greater than list length");
+            refPtr = refPtr.next;
+            count++;
+        }
+        while (refPtr != null) {
+            refPtr = refPtr.next;
+            mainPtr = mainPtr.next;
+        }
+        return mainPtr;
+    }
+
+    /*List should be sorted to make it work*/
+    public void removeDuplicates() {
+        if (isEmpty()) return;
+        Node current = head;
+        while (current.next != null) {
+            if (current.data == current.next.data)
+                current.next = current.next.next;
+            else
+                current = current.next;
+        }
+    }
+
+    private int length() {
+        int count = 0;
+        Node current = head;
+        while (current != null) {
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
+
+    public Node merge(Node first, Node second) {
+        Node dummy = new Node(0);
+        Node tail = dummy;
+        while (first != null && second != null) {
+            if (first.data <= second.data) {
+                tail.next = first;
+                first = first.next;
+            } else {
+                tail.next = second;
+                second = second.next;
+            }
+            tail = tail.next;
+        }
+        if (first == null) {
+            tail.next = second;
+        } else {
+            tail.next = first;
+        }
+        return dummy.next;
+    }
+
+    public Node sum(Node a, Node b) {
+        Node dummy = new Node(0);
+        Node tail = dummy;
+        int carry = 0;
+        while (a != null || b != null) {
+            int x = (a != null) ? a.data : 0;
+            int y = (b != null) ? b.data : 0;
+            int sum = carry + x + y;
+            tail.next = new Node(sum % 10);
+            carry = sum / 10;
+            tail = tail.next;
+            if (a != null) a = a.next;
+            if (b != null) b = b.next;
+        }
+        if (carry > 0) {
+            tail.next = new Node(carry);
+        }
+        return dummy.next;
+    }
+
+    public void print(Node head) {
         Node current = head;
         while (current != null) {
             System.out.print(current.data + "->");
@@ -104,9 +287,34 @@ public class SinglyLinkedList {
 
     public static void main(String[] args) {
         SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
-        singlyLinkedList.insertEnd(10);
-        singlyLinkedList.insertEnd(20);
-        singlyLinkedList.delete(10);
-        singlyLinkedList.print();
+
+        SinglyLinkedList first = new SinglyLinkedList();
+        first.insertEnd(1);
+        first.insertEnd(4);
+        first.insertEnd(6);
+        SinglyLinkedList second = new SinglyLinkedList();
+        second.insertEnd(2);
+        second.insertEnd(5);
+        second.insertEnd(8);
+        SinglyLinkedList merged = new SinglyLinkedList();
+        merged.print(merged.sum(first.head, second.head));
+    }
+
+    public void createLoopedLinkedList() {
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        Node node5 = new Node(5);
+        Node node6 = new Node(6);
+        Node node7 = new Node(7);
+        head = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+        node6.next = node7;
+        node7.next = node3;
     }
 }
